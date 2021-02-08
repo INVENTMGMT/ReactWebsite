47b139a105d457c517ab4b1c1dbb7ab1661ffa64
Samuel Gross <sgross02@calpoly.edu>
2021-02-08 11:12:24 -0700

    Base files for website

--- src/App.js
+++ src/App.js
@@ -1,68 +1,16 @@
-import React, { Component } from 'react'
-import Table from './Table.js'
-import Form from './Form.js';
-import FetchDemo from './FetchDemo.js'
-import axios from 'axios'
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
-class App extends Component {
-   state = { 
-     characters: []
-   };
-
-  removeCharacter = id => {
-    const { characters } = this.state
-    axios.delete('http://localhost:5000/users/' + id)
-      .then( res => {
-        console.log(res);
-        const characters = res.data.users_list;
-        this.setState({ characters });
-      })
-      .catch(function (error) {
-        // Not handling, just logging
-        console.log(error);
-      });
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:5000/users')
-      .then(res => {
-        const characters = res.data.users_list;
-        this.setState({ characters });
-      })
-      .catch(function (error) {
-        //Not handling, just logging
-        console.log(error);
-      });
-  }
-
-  makePostCall(character) {
-    return axios.post('http://localhost:5000/users', character)
-      .then(function (response) {
-        this.setState({ 'characters':response.data.data.users_list });
-        return (response.status == 201);
-      }.bind(this))
-      .catch(function (error) {
-        console.log(error);
-        return false;
-      });
-  }
-
-  handleSubmit = character => {
-    this.makePostCall(character);  }
-
-  render() {
-    const { characters } = this.state;
-
-    return (
-      <div className="Container">
-        <Table characterData={characters} removeCharacter={this.removeCharacter}/>
-        <Form handleSubmit={this.handleSubmit} />
-        <FetchDemo subreddit={"calpoly"}/> 
-      </div>
-    )
-  }
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
 }
-
 
 
 export default App;
