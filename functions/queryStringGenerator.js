af9821313eb7a64fae005a11aa1136437ac9a95f
Keon <kroohpar@gmail.com>
2021-03-01 10:50:25 -0800

    Work on queryStringGen

--- functions/queryStringGenerator.js
+++ functions/queryStringGenerator.js
@@ -5,10 +5,57 @@
 USAGE:
 
     functionName: the name of the desired function you want to call.
-        Available Function Names:
-            - getBy
+        Available Functions:
+            
+        - addItem: Function to create items and store them into backend
+            Params:
+                - Item: Object of the following form:
+                { id: String, name: String, price: Int, quantity: Int }
+            
+        - getAllItems: Function that will get all items that are in the database
+            Params:
+                - No parameters
 
+        - getByName: Function to search through Items via its name
+            Params:
+                - Name: String
+
+        - getByID: Function to search through Items via its ID
+            Params:
+                - id: String
+
+        - deleteItem: Function to delete an item with a matching ID
+            Params:
+                - id: String
 
 */
 
-const queryStringGenerator(functionName, id=None, name=None, )
+function queryStringGenerator(functionName, params) {
+    // Switch statement to determine what function the query string will be for
+    switch(str(functionName)) {
+        case "addItem":
+            console.log("add")
+            break;
+
+        case "getAllItems":
+            console.log("getAll")
+            break;
+            
+        case "getByName":
+            console.log("getName")
+            break;
+
+        case "getByID":
+            console.log("getId")
+            break;
+            
+        case "deleteItem":
+            console.log("del")
+            break;
+        
+        default:
+            throw "Incorrectly called queryStringGenerator(), functionName not found"
+    }
+}
+
+queryStringGenerator("getByID")
