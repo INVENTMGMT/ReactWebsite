c4c321f7c1f3307add126e3f08ef16f2a993b659
Keon <kroohpar@gmail.com>
2021-03-01 10:31:55 -0800

    Created file for queryStringGen

---
+++ functions/queryStringGenerator.js
@@ -0,0 +1,14 @@
+// This is a function that will create Query strings that will pass to to the backend to call backend
+// functions. These must be done by passing a query string via a post request to the backend endpoint.
+
+/*
+USAGE:
+
+    functionName: the name of the desired function you want to call.
+        Available Function Names:
+            - getBy
+
+
+*/
+
+const queryStringGenerator(functionName, id=None, name=None, )
