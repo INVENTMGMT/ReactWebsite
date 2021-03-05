f24a44b8e4eb03c4f507f485ecb8c47bf3e44110
Keon Roohparvar <kroohpar@gmail.com>
2021-03-04 19:07:50 -0800

    Function working properly

--- functions/backendFunction.js
+++ functions/backendFunction.js
@@ -2,7 +2,7 @@
 // functions. These must be done by passing a query string via a post request to the backend endpoint.
 
 import axios from 'axios'
-const endPoint = 'localhost://8000'
+const endPoint = 'http://localhost:3000/dev/graphql';
 
 /*
 USAGE:
@@ -49,21 +49,22 @@ USAGE:
         - deleteItem Params:
             - id: String        
 */
-
-function queryStringGenerator(functionName, params) {
+function backendFunction(functionName, params) {
 
     // Handling the issue if params is not passed in
     params = params || 0
 
-    // Creating the start of the query string, which will be passed into the backend via a post request
-    let queryStr = `{ "query": "{ `
+    // Creating the start of the query Object, which will be passed into the backend via a post request
+    let queryObj = {"query": null}
+    let queryStr = "{ "
 
     // Creating the string at the end of each GraphQL Post request, which makes GraphQL happy
-    var strEnd = ` {id name price quantity} }" }`
+    var strEnd = ` {id name price quantity} }`
 
     // Switch statement to determine what function the query string will be for
     switch(String(functionName)) {
         case "addItem":
+        {
             // Formatting params
             let { id, name, price, quantity} = params
             if (!id || !name || !price || !quantity) {
@@ -74,76 +75,109 @@ function queryStringGenerator(functionName, params) {
             queryStr += `addItem(id:"${id}", name:"${name}", price:${price}, quantity:${quantity})`
             queryStr += strEnd
             
-            console.log(queryStr)
+            // console.log(queryStr)
             break;
+        }
 
         case "getAllItems":
+        {   
             // Adding function header to query string & formatting it properly
-            queryStr += `addItem(id:"${id}", name:"${name}", price:${price}, quantity:${quantity})`
+            queryStr += `getAllItems `
             queryStr += strEnd
             
-            console.log(queryStr)
+            // console.log(queryStr)
             break;
-            
+        }
+
         case "getByName":
+        {
             // Formatting params
-            let { id, name, price, quantity} = params
-            if (!id || !name || !price || !quantity) {
+            let { name } = params
+            if (!name) {
                 throw "Incorrectly formatted params"
             }
 
             // Adding function header to query string & formatting it properly
-            queryStr += `addItem(id:"${id}", name:"${name}", price:${price}, quantity:${quantity})`
+            queryStr += `getByName(name:"${name}")`
             queryStr += strEnd
             
-            console.log(queryStr)
+            // console.log(queryStr)
             break;
+        }
 
         case "getByID":
+        {
             // Formatting params
-            let { id, name, price, quantity} = params
-            if (!id || !name || !price || !quantity) {
+            let { id } = params
+            if (!id) {
                 throw "Incorrectly formatted params"
             }
 
             // Adding function header to query string & formatting it properly
-            queryStr += `addItem(id:"${id}", name:"${name}", price:${price}, quantity:${quantity})`
+            queryStr += `getByID(id:"${id}")`
             queryStr += strEnd
             
-            console.log(queryStr)
+            // console.log(queryStr)
             break;
+        }
             
         case "deleteItem":
+        {
             // Formatting params
-            let { id, name, price, quantity} = params
-            if (!id || !name || !price || !quantity) {
+            let { id } = params
+            if (!id) {
                 throw "Incorrectly formatted params"
             }
 
             // Adding function header to query string & formatting it properly
-            queryStr += `addItem(id:"${id}", name:"${name}", price:${price}, quantity:${quantity})`
+            queryStr += `deleteItem(id:"${id}")`
             queryStr += strEnd
             
-            console.log(queryStr)
+            // console.log(queryStr)
             break;
-        
+        }
+
         default:
-            throw "Incorrectly called queryStringGenerator(), functionName not found"
+            throw `Function Name "${functionName}" not found - Make sure function name is spelled correctly.`
     }
 
-    /*
+    // Finalizing Query Object with the Query String as its data, which specifies the 
+    queryObj["query"] = queryStr
+    
     // Making POST request with query string
-    let resp = null
-    axios.post(endPoint, queryStr)
+    return axios.post(endPoint, queryObj)
         .then((response) => {
-            console.log(response);
-            resp = response
+            // console.log(`\nThis is response.data: ${response.data}\n and: ${JSON.stringify(response.data)}\n\n `)
+            return response.data["data"][functionName]
         }, (error) => {
             console.error(error)
         })
-    */
+        .then((respObject) => {
+            // console.log(respObject);
+            // console.log(`This is resp: ${respObject}\n and: ${JSON.stringify(respObject)}`)
+            return respObject;
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
     
 }
 
-let testParams = { id: "suck", name:"food", price:3, quantity:4 }
-queryStringGenerator("addItem", testParams)
+var testParams;
+var i;
+// for ( i=0; i < 5; i++)
+// {
+//     testParams = { id: `succ${i}`, name:`succ${i}`, price:3, quantity:4 }
+//     let resp = await backendFunction("getAllItems", testParams)
+//     console.log(resp)
+// }
+
+i=3
+testParams = { id: `succ${i}`, name:`succ${i}`, price:3, quantity:4 }
+let resp = await backendFunction("getByName", testParams)
+console.log(resp)
+
+export default backendFunction
+
+
